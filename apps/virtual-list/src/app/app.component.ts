f47fa5761e1c23cf09b13d4f74945fe814b37906
ban117 <bcam117@gmail.com>
2023-03-07 09:11:42 +0100

    chore(prettier): run

--- apps/virtual-list/src/app/app.component.ts
+++ apps/virtual-list/src/app/app.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'ban-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+	selector: "ban-root",
+	templateUrl: "./app.component.html",
+	styleUrls: ["./app.component.scss"],
 })
 export class AppComponent {
-  title = 'virtual-list';
+	title = "virtual-list";
 }
