3a298ce6065a974a566fbf7d563e25cf15ce82ea
ban117 <bcam117@gmail.com>
2023-03-08 09:45:40 +0100

    deps(material): add

--- apps/virtual-list/src/app/app.module.ts
+++ apps/virtual-list/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
 import { AppComponent } from "./app.component";
 import { NxWelcomeComponent } from "./nx-welcome.component";
@@ -13,6 +14,7 @@ import { appRoutes } from "./app.routes";
 		RouterModule.forRoot(appRoutes, {
 			initialNavigation: "enabledBlocking",
 		}),
+		BrowserAnimationsModule,
 	],
 	providers: [],
 	bootstrap: [AppComponent],
